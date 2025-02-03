cd837ca25e372e2674f79cb937e9789131596c86
이민우 <imin-u@imin-uui-MacBookPro.local>
2025-02-03 20:10:52 +0900

    feat: theme-provider
    - 패키지 설치
    - theme-provider 프로바이더

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
 import './globals.css';
+import { ThemeProvider } from '@/provider/theme-provider';
 
 const geistSans = Geist({
  variable: '--font-geist-sans',
@@ -24,8 +25,12 @@ export default function RootLayout({
  children: React.ReactNode;
 }>) {
  return (
-  <html lang="en">
-   <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+  <html lang="ko" suppressHydrationWarning>
+   <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+     {children}
+    </ThemeProvider>
+   </body>
   </html>
  );
 }
